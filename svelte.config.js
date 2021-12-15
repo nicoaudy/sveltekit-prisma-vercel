6f4c21a3f011cd27e33fe62f522d853f959b4b16
Nico Audy <nicoaudy1994@gmail.com>
2021-12-15 10:04:15 +0700

    ++ vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: vercel(),
 		target: '#svelte'
 	}
 };
