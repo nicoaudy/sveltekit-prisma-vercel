c2094b34affeaa2c5f13d8d20ccfcb4df5f87885
Nico Audy <nicoaudy1994@gmail.com>
2021-12-15 10:05:49 +0700

    ++ footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -60,3 +60,12 @@
 		</li>
 	{/each}
 </ul>
+<footer>
+	<p>
+		Created with <a class="link" target="_blank" rel="noopener" href="https://svelte.dev"
+			>SvelteKit</a
+		>
+		and deployed with
+		<a class="link" target="_blank" rel="noopener" href="https://vercel.com">▲ Vercel</a>.
+	</p>
+</footer>
